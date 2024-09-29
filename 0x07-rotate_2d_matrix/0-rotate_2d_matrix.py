bcb984b3b6aed7ee30645723d0b29de465a12ede
Victor <gambikimathi@gmail.com>
2024-09-29 14:05:36 -0700

    minor corrections on transpose file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
 
 def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
     matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
 
